df2a2352f5a3958aa9ab6004cb0027fda1783962
JoaoMiguelCardoso <jocardoso2010@gmail.com>
2021-09-30 16:52:28 -0300

    varivelo

--- SliceAll/Assets/Script/giraFrutas.cs
+++ SliceAll/Assets/Script/giraFrutas.cs
@@ -6,6 +6,8 @@ public class giraFrutas : MonoBehaviour
 {
     private Transform tr;
     [SerializeField]private Vector3 rodando;
+    private float velo;
+    public float varivelo;
     [SerializeField]private float setRodaTempo1, setRodaTempo2, setRodaTempo3;
     private float rodaTempo;
     void Start()
@@ -16,6 +18,7 @@ public class giraFrutas : MonoBehaviour
     }
     void FixedUpdate()
     {
+        rodando.z = velo +varivelo;
         tr.Rotate(rodando, Space.World);
         if(rodaTempo <= Time.time){
             mudaRotacao();
@@ -23,16 +26,16 @@ public class giraFrutas : MonoBehaviour
     }
 
     private void mudaRotacao(){
-        rodando.z = Random.Range(-6f, 6f);
-        if(rodando.z == 6 || rodando.z == -6){
+        velo = Random.Range(-6f, 6f);
+        if(velo == 6 || velo  == -6){
             rodaTempo = setRodaTempo1 + Time.time;
-        }else if(rodando.z > -6 && rodando.z < -4  || rodando.z < 6 && rodando.z > 4 ){
+        }else if(velo  > -6 && velo  < -4  || velo  < 6 && velo  > 4 ){
             rodaTempo = setRodaTempo2 + Time.time;
-        }else if(rodando.z >= -4 && rodando.z < -3  || rodando.z <= 4 && rodando.z > 3 ){
+        }else if(velo  >= -4 && velo  < -3  || velo  <= 4 && velo  > 3 ){
             rodaTempo = setRodaTempo3 + Time.time;
-        }else if(rodando.z >= -3 && rodando.z <= -1  || rodando.z <= 3 && rodando.z >= 1 ){
+        }else if(velo  >= -3 && velo  <= -1  || velo  <= 3 && velo  >= 1 ){
             rodaTempo = setRodaTempo2 + Time.time;
-        }else if(rodando.z > -1 && rodando.z < 1 ){
+        }else if(velo > -1 && velo  < 1 ){
             mudaRotacao();
         }
     }
