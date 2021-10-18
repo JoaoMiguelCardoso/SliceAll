2bbe9575c8fb501054a29bce1c48fe090642bb3e
JoaoMiguelCardoso <jocardoso2010@gmail.com>
2021-10-18 17:37:56 -0300

    clips

--- SliceAll/Assets/Script/cortaFruta.cs
+++ SliceAll/Assets/Script/cortaFruta.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class cortaFruta : MonoBehaviour
 {
+    public AudioClip[] audios;
     public AudioSource CortaFrutaSom;
     private void OnTriggerEnter2D(Collider2D other){
         if(other.tag.Equals("Faca")){
             GetComponentInParent<Contafruta>().frutacortada();
             Destroy(this.gameObject);
+            int o = Random.Range(0, audios.Length);
+            CortaFrutaSom.clip = audios[o];
             CortaFrutaSom.Play();
         }
     }
