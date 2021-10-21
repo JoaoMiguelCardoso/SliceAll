ad2ac41a50a82157eb3e0b43f6c7f3eafb104e64
JoaoMiguelCardoso <jocardoso2010@gmail.com>
2021-10-21 17:23:35 -0300

    Acerto do titulo e btm do som

--- SliceAll/Assets/Opcoes_BTM.cs
+++ SliceAll/Assets/Opcoes_BTM.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Opcoes_BTM : MonoBehaviour
 {
-    [SerializeField]private GameObject opcoes, creditos, botoes, fundo;
+    [SerializeField]private GameObject opcoes, creditos, botoes, fundo, BTM_Som;
+    [SerializeField]private Sprite[] som;
     bool ativo = true;
     public void AbreOpcoes(){
         Time.timeScale = 0f;
@@ -32,9 +33,11 @@ public class Opcoes_BTM : MonoBehaviour
     public void BotaoSom(){
         if(ativo){
             DesativaSom();
+            BTM_Som.GetComponent<Image>().sprite = som[1];
             ativo = false;
         }else{
             AtivaSom();
+            BTM_Som.GetComponent<Image>().sprite = som[0];
             ativo = true;
         }
     }
